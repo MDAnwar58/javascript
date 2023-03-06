a5a3854b499df10caa5dce076c1147d88d7b4f31
MD Anwar Sayeed <anwar.saeed656@gmail.com>
2023-03-06 17:36:54 +0600

    javascript-1

---
+++ arrow.js
@@ -0,0 +1,24 @@
+simple js function
+var a = function (n)
+{
+  return n;
+}
+
+console.log(a('Learn Hunter'));
+
+arrow function
+let fruit = (f) => { return f }
+console.log(fruit('Apple'));
+
+arrow function
+let fruit = f => f;
+console.log(fruit('Mango'));
+
+arrow multiply function
+let multiply = m => m*m*m;
+console.log(multiply('3'));
+
+
+arrow function's when multiple variable then apply it
+let gun = (m, n) => { return m * n
+console.log(gun(3, 4));
